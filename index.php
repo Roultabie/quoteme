1c9c677be70b537583ef45c0b06b3bb613b05765
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-05-29 17:12:11 +0200

    minor bugfixes

--- index.php
+++ index.php
@@ -2,16 +2,22 @@
 /**
  * Loading libs
  */
+require_once 'libs/mysql.php';
 require_once 'libs/quoteme.php';
 require_once 'libs/timply.php';
 require_once 'libs/smartypants.php';
+
 define('TIMPLY_DIR', 'themes/simple/');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'quoteme');
+define('DB_USR', 'user');
+define('DB_PWD', 'pass');
 
 $html  = new timply('index.html');
 $quote = new quoteQueries();
 $quote = $quote->getQuote();
-$html->setElement('text', SmartyPants($quote[0]->getText()));
-$html->setElement('author', SmartyPants($quote[0]->getAuthor()));
-$html->setElement('source', SmartyPants($quote[0]->getSource()));
+$html->setElement('text', SmartyPants($quote[0]->getText(), 'f+:+t+h+H+'), 'Content');
+$html->setElement('author', SmartyPants($quote[0]->getAuthor()), 'Content');
+$html->setElement('source', SmartyPants($quote[0]->getSource()), 'Content');
 echo $html->returnHtml();
 ?>
