849b0b241db8033cb55cc698f1e8cb4fcd6dce3c
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-09 01:16:06 +0200

    fixed bug with non array datas

--- parser/rss2.php
+++ parser/rss2.php
@@ -16,11 +16,13 @@ implements parserTemplate
     {
         $this->timply->setElement('title', 'Q.uote.me RSS feed');
         $this->timply->setElement('description', 'Q.uote.me is a quote random generator');
-        foreach ($elements as $value) {
-            $this->timply->setElement('itemTitle', $value->getAuthor() . ' said', 'Item');
-            $this->timply->setElement('itemLink', '', 'Item');
-            $this->timply->setElement('itemDescription', $value->getText(), 'Item');
-            $this->timply->setElement('nbQuotes', $nbQuotes);
+        if (is_array($elements)) {
+            foreach ($elements as $value) {
+                $this->timply->setElement('itemTitle', $value->getAuthor() . ' said', 'Item');
+                $this->timply->setElement('itemLink', '', 'Item');
+                $this->timply->setElement('itemDescription', $value->getText(), 'Item');
+                $this->timply->setElement('nbQuotes', $nbQuotes);
+            }
         }
         return $this->timply->returnHtml();
     }
