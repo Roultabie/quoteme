f30cb473d4eed164d9ae2c0233e6bcb0cf992c9a
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-09 22:32:35 +0200

    modified to use api.php

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <?php
 /**
+ * Loading parser
+ */
+require_once 'api.php';
+
+/**
  * Loading libs
  */
-require_once 'libs/mysql.php';
-require_once 'libs/quoteme.php';
-require_once 'libs/timply.php';
 require_once 'libs/smartypants.php';
 
 define('TIMPLY_DIR', 'themes/simple/');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'quoteme');
-define('DB_USR', 'dbuser');
-define('DB_PWD', 'pass');
 
 $html     = new timply('index.html');
-$quote    = new quoteQueries();
-$nbQuotes = quoteQueries::$nbQuotes;
-$quote    = $quote->getQuote();
-$html->setElement('text', SmartyPants($quote[0]->getText(), 'f+:+t+h+H+'));
-$html->setElement('author', SmartyPants($quote[0]->getAuthor()));
-$html->setElement('source', SmartyPants($quote[0]->getSource()));
-$html->setElement('nbQuotes', $nbQuotes);
+$quote = $GLOBALS['quoteObj']['obj'][0];
+if (is_object($quote)) {
+    $html->setElement('text', SmartyPants($quote->getText(), 'f+:+t+h+H+'));
+    $html->setElement('author', SmartyPants($quote->getAuthor()));
+    $html->setElement('source', SmartyPants($quote->getSource()));
+}
+$html->setElement('nbQuotes', $GLOBALS['quoteObj']['nb']);
 echo $html->returnHtml();
 ?>
