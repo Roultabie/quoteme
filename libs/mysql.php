54e9b1ed207310dc320d0e2e80c28b9d88cd4b65
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-10 13:37:27 +0200

    updated code

--- libs/mysql.php
+++ libs/mysql.php
@@ -5,22 +5,40 @@
 class dbConnexion
 {
     private static $instance;
-    /*function __construct(argument)
+    private static $dbHost;
+    private static $dbName;
+    private static $dbUser;
+    private static $dbPass;
+
+    function __construct()
     {
-        # code...
-    }*/
+        self::setDbInfos();
+    }
 
     public static function getInstance(){
         if(!isset(self::$instance)){
-            try {
-                self::$instance = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USR, DB_PWD);
-                self::$instance->query("SET NAMES 'utf8'");
-            } catch (Exception $e) {
-                print "Erreur !: " . $e->getMessage() . "<br/>";
-                die();
-            }
+            self::setInstance();
         }
         return self::$instance;
+    }
+
+    private static function setInstance()
+    {
+        try {
+            self::$instance = new PDO('mysql:host=' . self::$dbHost . ';dbname=' . self::$dbName, self::$dbUser, self::$dbPass);
+            self::$instance->query("SET NAMES 'utf8'");
+        } catch (Exception $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    private static function setDbInfos()
+    {
+        self::$dbHost = $GLOBALS['config']['dbHost'];
+        self::$dbName = $GLOBALS['config']['dbName'];
+        self::$dbUser = $GLOBALS['config']['dbUser'];
+        self::$dbPass = $GLOBALS['config']['dbPass'];
     }
 }
 ?>
