ba3f55dff9c02b8c5c008d07598fe6ea5710cb38
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-10 13:47:31 +0200

    removed old configuration

--- config.php
+++ config.php
@@ -6,11 +6,4 @@ $config['dbPass'] = 'pass';
 $config['themeDir'] = 'themes/simple/';
 $config['siteDoc'] = 'http://q.uote.me/api.php';
 $config['siteContact'] = 'contact@aelys-info.fr';
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'quoteme');
-define('DB_USR', 'dbuser');
-define('DB_PWD', 'pass');
-define('TIMPLY_DIR', 'themes/simple/');
-define('SITE_DOC', 'http://q.uote.me/api.php');
-define('SITE_CONTACT', 'contact@aelys-info.fr');
 ?>
