9194954ad254f489c6abafd1f416289c2ab5fad4
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-14 11:29:17 +0200

    added lang option

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ $config['dbHost'] = 'localhost';
 $config['dbName'] = 'quoteme';
 $config['dbUser'] = 'dbuser';
 $config['dbPass'] = 'pass';
+$config['lang'] = 'fr_FR';
 $config['themeDir'] = 'themes/simple/';
 $config['siteDoc'] = 'http://q.uote.me/api.php';
 $config['siteContact'] = 'contact@aelys-info.fr';
