b9261501b08cec71252214b8912581d2aee2e682
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-14 11:28:43 +0200

    added permalink management and parse options

--- parser/parser.php
+++ parser/parser.php
@@ -3,7 +3,7 @@ abstract class parser
 {
     function __construct()
     {
-        # 
+        
     }
 
     final protected function isUTF8($text)
@@ -14,6 +14,22 @@ abstract class parser
     final protected function toUTF8($text)
     {
         #
+    }
+
+    final protected function returnUri()
+    {
+        return $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+    }
+
+    final protected function returnScriptUri()
+    {
+        return $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+    }
+
+    final protected function returnPermalink($permalink)
+    {
+        return $_SERVER['HTTP_HOST'] . '/?' . $permalink;
     }
 }
 ?>
--- parser/rss2.php
+++ parser/rss2.php
@@ -14,16 +14,33 @@ implements parserTemplate
     public function parse($elements)
     {
         $this->timply->setElement('title', 'Q.uote.me RSS feed');
+        $this->timply->setElement('link', $this->returnScriptUri()); // WIP
         $this->timply->setElement('description', 'Q.uote.me is a quote random generator');
+        $this->timply->setElement('language', $GLOBALS['config']['lang']);
+        $this->timply->setElement('docs', $GLOBALS['config']['siteDoc']);
         if (is_array($elements)) {
             foreach ($elements as $value) {
                 $this->timply->setElement('itemTitle', $value->getAuthor() . ' said', 'Item');
-                $this->timply->setElement('itemLink', '', 'Item');
+                $this->timply->setElement('itemLink', $this->returnPermalink($value->getPermalink()), 'Item');
                 $this->timply->setElement('itemDescription', $value->getText(), 'Item');
-                $this->timply->setElement('nbQuotes', $nbQuotes);
+                $this->timply->setElement('itemPubDate', $this->formatDate($value->getDate()), 'Item');
+                $this->timply->setElement('itemGuid', $this->returnPermalink($value->getPermalink()), 'Item');
+                // we take last date for pubdate
+                $dates[] = $value->getDate();
             }
+            $this->timply->setElement('pubDate', $this->formatDate(max($dates))); //WIP
         }
         return $this->timply->returnHtml();
+    }
+
+    private function formatDate($sqlDate)
+    {
+        $dateTime  = explode(' ', $sqlDate);
+        $date      = explode('-', $dateTime[0]);
+        $time      = explode('-', $dateTime[1]);
+        $timestamp = mktime($time[0], $time[1], $time[2], $date[1], $date[2], $date[0]);
+        $rssDate   = date(DATE_RSS, $timestamp);
+        return $rssDate;
     }
 }
 ?>
