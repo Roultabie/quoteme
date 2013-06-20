0b692cecb1281674bafcc269564801d2e3b0753e
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-20 13:38:42 +0200

    added simple login script (see my login repo)

---
+++ form.php
@@ -0,0 +1,5 @@
+<form name="login" method="post">
+    <input type="text" name="login"><br>
+    <input type="password" name="pass"><br>
+    <input type="submit" value="se connecter">
+</form>
---
+++ libs/login.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Login lib
+ */
+
+class user
+{
+
+    function __construct()
+    {
+
+    }
+
+    function getUsername()
+    {
+        return $this->username;
+    }
+
+    function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    function getMail()
+    {
+        return $this->Mail;
+    }
+
+    function setMail($Mail)
+    {
+        $this->Mail = $Mail;
+    }
+
+    function getDescription()
+    {
+        return $this->Description;
+    }
+
+    function setDescription($Description)
+    {
+        $this->Description = $Description;
+    }
+
+    function getAuth()
+    {
+        return $auth;
+    }
+
+    function setAuth($auth)
+    {
+        $this->auth = $auth;
+    }
+
+    function getIp()
+    {
+        return $this->ip;
+    }
+
+    function setIp($ip)
+    {
+        $this->ip = $ip;
+    }
+
+    function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+
+    function setUserAgent($userAgent)
+    {
+        $this->userAgent = $userAgent;
+    }
+}
+
+class userWriter
+{
+
+    function __construct()
+    {
+        $this->user     = $GLOBALS['config']['user'];
+        $this->password = $GLOBALS['config']['password'];
+    }
+    
+    function loginCheck($login = "", $password = "") // pour utiliser une autre méthode (sql ...) faire hériter une nouvelle classe et redéfinir cette méthode
+    {
+        if (!empty($login) && !empty($password)) {
+            if ($this->user === $login && $this->password === hash('sha256', $password)) {
+                $user = new user();
+                $user->setUsername($GLOBALS['config']['user']);
+                $user->setMail($GLOBALS['config']['mail']);
+                $user->setDescription($GLOBALS['config']['userInfo']);
+                $user->setIp($_SERVER['REMOTE_ADDR']);
+                $user->setUserAgent($_SERVER['HTTP_USER_AGENT']);
+                //$user->setAuth($this->setAuthorizations);
+                $_SESSION['userDatas'] = serialize($user);
+                return $user;
+            }
+            else {
+                unset($user); // On ne sait jamais ;)
+                return FALSE;
+            }
+        }
+        elseif(is_object(unserialize($_SESSION['userDatas']))) {
+            $user     = unserialize($_SESSION['userDatas']);
+            $lastHash = hash('sha256', $user->getIp() . $user->getUserAgent());;
+            //$currHash = hash('sha256', '10.10.10.10' . $_SERVER['HTTP_USER_AGENT']);
+            $currHash = hash('sha256', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']);
+            if ($lastHash === $currHash) {
+                return $user;
+            }
+            else {
+                unset($user);
+                return FALSE;
+            }
+        }
+        else {
+            unset($user);
+            return FALSE;
+        }
+    }
+
+    public static function initSession()
+    {
+        session_start();
+    }
+
+    public static function killSession()
+    {
+        session_destroy();
+    }
+}
+
+userWriter::initSession();
+$session = new userWriter();
+if (!empty($_POST['login']) && !empty($_POST['pass'])) {
+    $user = $_POST['login'];
+    $pass = $_POST['pass'];
+}
+$user = $session->loginCheck($user, $pass);
+if (!is_object($user) || $_POST['disconnect'] === '1' || $_GET['disconnect'] === '1') {
+    userWriter::killSession();
+    require 'form.php';
+    exit();
+}
+
+?>
