eb5edf024f815151457fcf8152bead77cca3d09e
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-20 13:44:52 +0200

    added user informations for libs/login.php

--- config.php
+++ config.php
@@ -7,4 +7,6 @@ $config['lang'] = 'fr_FR';
 $config['themeDir'] = 'themes/simple/';
 $config['siteDoc'] = 'http://q.uote.me/api.php';
 $config['siteContact'] = 'contact@aelys-info.fr';
+$config['user'] = 'dd';
+$config['password'] = 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1';
 ?>
