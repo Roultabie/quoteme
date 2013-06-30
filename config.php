34547577d79cd9fbbca43a2ce874ff6c83e45803
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-30 13:42:01 +0200

    sanitized config.php

--- config.php
+++ config.php
@@ -10,16 +10,16 @@ $system['lastUpdate'] = '0000-00-00';
  * config options, put your informations here
  */
 $config['dbHost'] = 'localhost';
-$config['dbName'] = 'quoteme';
-$config['dbUser'] = 'dbuser';
-$config['dbPass'] = 'pass';
-$config['lang'] = 'fr_FR';
+$config['dbName'] = '';
+$config['dbUser'] = '';
+$config['dbPass'] = '';
+$config['lang'] = 'en_EN';
 $config['themeDir'] = 'themes/simple/';
 $config['langDir'] = 'lang/';
 $config['siteDoc'] = 'http://q.uote.me/api.php';
-$config['siteContact'] = 'contact@aelys-info.fr';
-$config['user'] = 'demo';
-$config['password'] = '2a97516c354b68848cdbd8f54a226a0a55b21ed138e207ad6c5cbb9c00aa5aea';
+$config['siteContact'] = '';
+$config['user'] = '';
+$config['password'] = '';
 $config['sessionExpire'] = 1800;
 $config['appVers'] = $system['version']; // your app version
 //$config['appVers'] = 'a5e'; // (anonyme) Anonyme stat of client when check update
