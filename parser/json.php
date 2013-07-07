609d7978f05fbff705ca725a86a81a205b3fa317
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-07-07 11:13:12 +0200

    updated json parser with headers, thanks to Thomas Lleixa

--- parser/json.php
+++ parser/json.php
@@ -8,14 +8,23 @@ implements parserTemplate
         $i = 0;
         if (is_array($elements)) {
             foreach ($elements as $value) {
-                $quote[$i]['text']   = $value->getText();
-                $quote[$i]['author'] = $value->getAuthor();
-                $quote[$i]['source'] = $value->getSource();
+                $quotes[$i]['text']   = $value->getText();
+                $quotes[$i]['author'] = $value->getAuthor();
+                $quotes[$i]['source'] = $value->getSource();
                 $i++;
             }
+            $datas['status'] = 'success' ;
+            $datas['data']   = $quotes;
         }
-        $result = json_encode($quote);
-        return $result;
+        else {
+            $datas['status'] = 'error';
+            $datas['data']   = 'Error, no data found';
+        }
+        $result = json_encode($datas);
+        header('Cache-Control: no-cache, must-revalidate'); // Gestion du cache et optimisations
+        header('Expires: Ven, 11 Oct 2011 23:32:00 GMT');   // Limite la durée validité
+        header('Content-type: application/json');           // Über _IMPORTANT_
+        exit($result);
     }
 }
 ?>
