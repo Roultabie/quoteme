ebb8c839cce6143444db701b5e7abbb9402813aa
Daniel DOUAT <daniel.douat@aelys-info.fr>
2014-06-14 21:47:55 +0200

    added basic html parser

---
+++ parser/html.php
@@ -0,0 +1,48 @@
+<?php
+class htmlParser
+extends parser
+implements parserTemplate
+{
+
+    private $timply;
+
+    function __construct()
+    {
+        timply::setUri($GLOBALS['config']['themeDir']);
+        timply::setFileName('html.html');
+        timply::addDictionary($GLOBALS['config']['langDir'] . $GLOBALS['config']['lang'] . '.php');
+        $this->timply = new timply();
+    }
+
+    public function parse($elements)
+    {
+        $this->timply->setElement('title', 'Q.uote.me');
+        if (is_array($elements)) {
+            foreach ($elements as $value) {
+                $this->timply->setElement('text', $value->getText(), 'Quote');
+                $this->timply->setElement('permalink', $this->returnPermalink($value->getPermalink()), 'Quote');
+                $this->timply->setElement('description', $value->getText(), 'Quote');
+                $this->timply->setElement('author', $value->getAuthor(), 'Quote');
+            }
+        }
+        return $this->timply->returnHtml();
+    }
+
+    public function loadHeader($elements = '')
+    {
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: Ven, 07 Oct 2011 23:32:00 GMT');
+        header('Content-Type: text/html; charset=utf-8');
+    }
+
+    private function formatDate($sqlDate)
+    {
+        $dateTime  = explode(' ', $sqlDate);
+        $date      = explode('-', $dateTime[0]);
+        $time      = explode('-', $dateTime[1]);
+        $timestamp = mktime($time[0], $time[1], $time[2], $date[1], $date[2], $date[0]);
+        $rssDate   = date(DATE_RSS, $timestamp);
+        return $rssDate;
+    }
+}
+?>
