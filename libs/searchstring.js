40af19d3143e18c408a539fd336560ee256ba9c9
Daniel DOUAT <daniel.douat@aelys-info.fr>
2014-06-22 22:30:40 +0200

    resolved event problem

--- libs/searchstring.js
+++ libs/searchstring.js
@@ -10,76 +10,73 @@ function createRequestObject()
     return http;
 }
 
-function searchString(obj, dataType)
+function searchString(obj, dataType, event)
 {
-    obj.onkeyup = function(event) {
-        //console.log(event.keyCode);
-        var currentKey = event.keyCode;
-        obj.setAttribute("autocomplete", "off");
-        var inputContent = obj.value;
-        //console.log(inputContent.search(/,$/g));
-        if (inputContent.search(',') !== -1) {
-            var elements = inputContent.split(',');
-            var toSend   = elements.pop().replace(/^\s+/g,'');
-            // Si on a une , c'est qu'on a un nouveau tag, donc on calcul la position des suggestions
-            if (currentKey === 188 || currentKey === 8 && inputContent.search(/,$/g) != -1) {
-                if (currentKey === 188) {
-                    calculateBubblePosition(obj, 0);
-                }
-                else {
-                    calculateBubblePosition(obj,1);
-                };
+    var currentKey = event.keyCode;
+    obj.setAttribute("autocomplete", "off");
+    var inputContent = obj.value;
+    //console.log(inputContent.search(/,$/g));
+    if (inputContent.search(',') !== -1) {
+        var elements = inputContent.split(',');
+        var toSend   = elements.pop().replace(/^\s+/g,'');
+        // Si on a une , c'est qu'on a un nouveau tag, donc on calcul la position des suggestions
+        if (currentKey === 188 || currentKey === 8 && inputContent.search(/,$/g) != -1) {
+            if (currentKey === 188) {
+                calculateBubblePosition(obj, 0);
+            }
+            else {
+                calculateBubblePosition(obj, 1);
             };
-        }
-        else {
-            var elements = [];
-            var toSend   = inputContent.replace(/^\s+/g,'');
         };
-        var http = createRequestObject();
-        http.open('GET', '/admin.php?' + dataType + '=' + toSend, true);
-        http.onreadystatechange = ( function ()
-        {
-            if (http.readyState === 4) {
-                if (http.status === 200) {
-                    var result = eval( '(' + http.responseText + ')' );
-                    if (result !== false) {
-                        if (document.getElementById(obj.id + 'suggest') === null) {
-                            var ul            = document.createElement('ul');
-                            ul.id             = obj.id + 'suggest';
-                            ul.style.position = 'absolute';
-                            ul.style.top      = eval(obj.offsetTop + obj.offsetHeight) + 'px';
-                            var parent        = obj.parentNode;
-                            parent.insertBefore(ul, obj);
+    }
+    else {
+        var elements = [];
+        var toSend   = inputContent.replace(/^\s+/g,'');
+    };
+    var http = createRequestObject();
+    http.open('GET', '/admin.php?' + dataType + '=' + toSend, true);
+    http.onreadystatechange = ( function ()
+    {
+        if (http.readyState === 4) {
+            if (http.status === 200) {
+                var result = eval( '(' + http.responseText + ')' );
+                if (result !== false) {
+                    if (document.getElementById(obj.id + 'suggest') === null) {
+                        var ul            = document.createElement('ul');
+                        ul.id             = obj.id + 'suggest';
+                        ul.style.position = 'absolute';
+                        ul.style.top      = eval(obj.offsetTop + obj.offsetHeight) + 'px';
+                        var parent        = obj.parentNode;
+                        parent.insertBefore(ul, obj);
+                    };
+                    
+                    document.getElementById(obj.id + 'suggest').innerHTML = '';
+                    if (result['status'] === 'success') {
+                        for(var i= 0; i < result.data.length; i++)
+                        {
+                            var li = document.createElement('li');
+                            li.id  = obj.id + 'li' + i;
+                            document.getElementById(obj.id + 'suggest').appendChild(li);
+                            var a           = document.createElement('a');
+                            a.innerHTML     = result.data[i].value;
+                            a.name          = obj.id + 'a' + i
+                            a.style.display = 'block';
+                            a.onclick       = function() {
+                                var parent  = this.parentNode;
+                                // On concat la valeur cliquée au tableau de l'input
+                                elements.push(this.innerHTML);
+                                obj.value = elements.join(',');
+                                document.getElementById(obj.id + 'suggest').innerHTML = '';
+                            };
+                            document.getElementById(obj.id + 'li' + i).appendChild(a);
                         };
                         
-                        document.getElementById(obj.id + 'suggest').innerHTML = '';
-                        if (result['status'] === 'success') {
-                            for(var i= 0; i < result.data.length; i++)
-                            {
-                                var li = document.createElement('li');
-                                li.id  = obj.id + 'li' + i;
-                                document.getElementById(obj.id + 'suggest').appendChild(li);
-                                var a           = document.createElement('a');
-                                a.innerHTML     = result.data[i].value;
-                                a.name          = obj.id + 'a' + i
-                                a.style.display = 'block';
-                                a.onclick       = function() {
-                                    var parent  = this.parentNode;
-                                    // On concat la valeur cliquée au tableau de l'input
-                                    elements.push(this.innerHTML);
-                                    obj.value = elements.join(',');
-                                    document.getElementById(obj.id + 'suggest').innerHTML = '';
-                                };
-                                document.getElementById(obj.id + 'li' + i).appendChild(a);
-                            };
-                            
-                        };
                     };
                 };
             };
-        } );
-        http.send(null);
-    };            
+        };
+    } );
+    http.send(null);
 };
 
 function calculateBubblePosition(obj, remove)
