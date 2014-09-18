0ffd5392ae999a0e4e06b21dbb0670d9e32445b9
Daniel Douat <daniel.douat@aelys-info.fr>
2014-09-18 13:39:20 +0200

    updated code

--- libs/wget.js
+++ libs/wget.js
@@ -20,11 +20,9 @@ function changeQuote()
             if (http.status == 200) {
                 var quote = eval( '(' + http.responseText + ')' );
                 if (quote !== false) {
-                    var oldTitle                                  = document.title;
                     var oldAuthor                                 = document.getElementById('author').innerHTML;
                     var oldAuthorSearchLink                       = document.getElementById('author').href;
-                    var titleParts                                = oldTitle.split('-');
-                    document.title                                = quote.data[0]['author'] + ' -' + titleParts.pop();
+                    document.title                                = document.title.replace(oldAuthor, quote.data[0]['author']);
                     document.getElementById('textLink').innerHTML = quote.data[0]['text'];
                     document.getElementById('author').innerHTML   = quote.data[0]['author'];
                     document.getElementById('author').href        = oldAuthorSearchLink.replace(encodeURI(oldAuthor), quote.data[0]['author']);
