defb417bea0cf7c4c9c55bc1f85bc51129af565c
Daniel Douat <daniel.douat@aelys-info.fr>
2014-09-18 11:33:22 +0200

    added author search

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ if (is_object($quote)) {
     $html->setElement('imglink', '//' .$_SERVER['HTTP_HOST'] . '/api.php?p=img&wi=1024&w=permalink&wo=equal,' . $quote->getPermalink());
     $html->setElement('text', SmartyPants($quote->getText(), 'f+:+t+h+H+'));
     $html->setElement('author', SmartyPants($quote->getAuthor()));
+    $html->setElement('searchByAuthor', '//' .$_SERVER['HTTP_HOST'] . '/api.php?p=html&w=author&wo=equal,' . $quote->getAuthor());
     $html->setElement('source', SmartyPants($quote->getSource()));
     $html->setElement('permalink', '?' .$quote->getPermalink());
 }
