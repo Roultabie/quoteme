838be86d2c4367c16180ccb76c4e75355deac8d8
Daniel Douat <daniel.douat@aelys-info.fr>
2017-01-20 13:24:50 +0100

    added initial model for delivered stats

--- libs/apistats.php
+++ libs/apistats.php
@@ -1,6 +1,61 @@
 <?php
 
 # Models
+/**
+ *
+ */
+class statsQueries
+{
+
+    function __construct()
+    {
+        self::$tblPrefix  = $GLOBALS['config']['tblPrefix'];
+    }
+
+    function getDelivered($year = $month = $day = $user = '')
+    {
+        if (!empty($year)) {
+            if (count($year) < 4) return 400;
+            $dateSearch = $year . '-';
+        }
+        if (!empty($month)) {
+            if (count($month) < 2) return 400;
+            if (empty($year)) return 400;
+            $dateSearch .= $month . '-';
+        }
+        if (!empty($day)) {
+            if (count($day) < 2) return 400;
+            if (empty($month)) return 400;
+            $dateSearch .= $day;
+        }
+        $dateSearch .= '%'
+
+        if (!empty($user)) {
+            $query = 'SELECT ' . self::$tblPrefix . 'users.id,
+                      COUNT (' . self::$tblPrefix . 'delivered.id,) AS total
+                      FROM ' . self::$tblPrefix . 'users AS u
+                      INNER JOIN ' . self::$tblPrefix . 'delivered AS d
+                      ON u.share_token = d.share_token
+                      WHERE u.id = ":user"
+                      AND d.date LIKE ":dateSearch";';
+        }
+        else {
+            $query = 'SELECT date permalink source parser
+                      FROM ' . self::$tblPrefix . 'delivered
+                      WHERE date LIKE ":dateSearch"';
+        }
+        $stmt = dbConnexion::getInstance()->prepare($query);
+        if (!empty($user)) $stmt->bindValue(':user', $user, PDO::PARAM_STR);
+        $stmt->bindValue(':dateSearch', $dateSearch, PDO::PARAM_STR);
+        $stmt->execute();
+        $datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = NULL;
+        if (count($datas) > 0) {
+            if $datas[0]['total'] !== '0') return $datas[0]['total'];
+        }
+        return 404;
+    }
+}
 
 
 # Controllers
